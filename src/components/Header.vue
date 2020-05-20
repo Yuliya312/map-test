7613085d13d6dd69e3c17f1c616588fe56e67ce3
Yuliia Honcharenko <yuliya_312@ukr.net>
2020-05-20 22:26:37 +0300

    add address part

---
+++ src/components/Header.vue
@@ -0,0 +1,124 @@
+<template>
+  <header class="header">
+    <h1 class="header__title title__h1">Contacts</h1>
+    <div class="header__info">
+      <div class="header__contacts">
+        <h2 class="header__subtitle title__h2">
+          <a href="#" class="header__subtitle-link link link--big">GMS</a>
+          Headquarters
+        </h2>
+        <div class="header__headquaters">
+          <div class="header__adress">
+            <ul class="header__list">
+              <li class="header__list-item">
+                Global Message Services Ukraine LLC
+              </li>
+              <li class="header__list-item">Bundesstrasse 5 | 3rd Floor</li>
+              <li class="header__list-item">6300 Zug</li>
+              <li class="header__list-item">Switzerland</li>
+            </ul>
+            <a href="#" class="header__phone">
+              +41 41 544 62 00
+            </a>
+          </div>
+          <div class="header__contact contact">
+            <p class="contact__title">
+              Email
+            </p>
+            <a href="#" class="contact__email link link--small"
+              >info@gms-worldwide.com</a
+            >
+            <p class="contact__title">
+              Send message
+            </p>
+            <a href="#" class="contact__phone link link--small">Viber us</a>
+            <p class="contact__title">
+              Send form
+            </p>
+            <a href="#" class="contact__form link link--small">Contact us</a>
+          </div>
+        </div>
+      </div>
+      <div class="header__support">
+        <h2 class="header__subtitle title__h2">
+          Technikal support
+          <a href="#" class="header__subtitle-link link link--big">24/7</a>
+        </h2>
+        <p class="header__support-text">
+          GMS understands that it can be hard to follow all new technologies.
+          regularly provide for all our clients educational trainings to your
+          your operator`s business earn more and better though our best
+          practices.
+        </p>
+        <a href="#" class="header__phone">
+          +41 41 544 62 00
+        </a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss">
+.header {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 60px 60px 0;
+  background-color: $color-white;
+
+  &__title {
+    margin-bottom: 50px;
+  }
+
+  &__info {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin-bottom: 98px;
+  }
+
+  &__contacts {
+    max-width: 548px;
+
+    @include respond(tab-land) {
+      margin: 0 20px 40px 0;
+    }
+  }
+
+  &__headquaters {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+
+  &__adress {
+    max-width: 264px;
+    margin-right: 30px;
+  }
+
+  &__list {
+    margin-bottom: 40px;
+  }
+
+  &__phone {
+    color: $color-secondary;
+    font-size: 26px;
+    line-height: 36px;
+  }
+
+  &__contact {
+    max-width: 254px;
+  }
+
+  &__subtitle {
+    margin-bottom: 40px;
+  }
+
+  &__support {
+    max-width: 538px;
+  }
+
+  &__support-text {
+    margin-bottom: 40px;
+  }
+}
+</style>
---
+++ src/components/MainSection.vue
@@ -0,0 +1,165 @@
+<template>
+  <main class="main">
+    <section class="offices">
+      <div class="offices__info">
+        <h2 class="offices__title title__h2">Our Offices</h2>
+        <nav class="offices__nav navigation">
+          <ul class="navigation__list">
+            <li
+              class="navigation__item"
+              v-for="office in offices"
+              :key="office.id"
+            >
+              <button
+                class="navigation__link"
+                :class="{ 'link--active': currentOffice.city === office.city }"
+                @click="getCurrent(office)"
+              >
+                {{ office.city }}
+              </button>
+            </li>
+          </ul>
+          <p class="offices__company title__h4">
+            Global Message Services
+            <span class="offices__country">
+              {{ currentOffice.country }}
+            </span>
+            LLC
+          </p>
+          <ul class="offices__address">
+            <li class="offices__address-list">
+              {{ currentOffice.address }}
+            </li>
+            <li class="offices__address-list">
+              {{ currentOffice.postIndex }}
+            </li>
+            <li class="offices__address-list">
+              {{ currentOffice.country }}
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div class="offices__map"></div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      offices: [
+        {
+          id: 1,
+          city: "Kyiv",
+          address: "Stepan Bandera, 33",
+          postIndex: "02066",
+          country: "Ukraine"
+        },
+        {
+          id: 2,
+          city: "New York",
+          address: "Loan Street, 58",
+          postIndex: "33140",
+          country: "USA"
+        },
+        {
+          id: 3,
+          city: "Guangzhou",
+          address: "Tian He Bei Road, 233",
+          postIndex: "69854",
+          country: "China"
+        },
+        {
+          id: 4,
+          city: "Barcelona",
+          address: "Plaça Rosa Del Vents 1",
+          postIndex: "12345",
+          country: "Spain"
+        }
+      ],
+      currentOffice: {
+        city: "Kyiv",
+        id: 1,
+        address: "Stepan Bandera, 33",
+        postIndex: "02066",
+        country: "Ukkraine"
+      }
+    };
+  },
+  methods: {
+    getCurrent(office) {
+      this.currentOffice = office;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.main {
+  background-color: $color-primary;
+  color: $color-white;
+}
+
+.offices {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 0 0 0 60px;
+  display: flex;
+  flex-wrap: wrap;
+
+  &__info {
+    max-width: 580px;
+  }
+
+  &__title {
+    padding-top: 98px;
+    margin-bottom: 40px;
+  }
+
+  &__company {
+    margin-bottom: 40px;
+  }
+
+  &__address-list {
+    font-size: 11px;
+    line-height: 20px;
+    letter-spacing: 0.3;
+  }
+
+  &__map {
+    max-width: 640px;
+    min-height: 500px;
+  }
+}
+
+.navigation {
+  &__list {
+    display: flex;
+    flex-wrap: wrap;
+    margin-bottom: 20px;
+  }
+
+  &__link {
+    border: none;
+    background-color: $color-primary;
+    color: inherit;
+    font-size: 11px;
+    line-height: 72.34px;
+    transition: text-transform 0.3s, color 0.3s;
+
+    &:hover {
+      color: $color-secondary;
+    }
+
+    &:active {
+      transform: scale(1.3);
+    }
+  }
+}
+
+.link--active {
+  color: $color-secondary;
+  text-transform: uppercase;
+}
+</style>
